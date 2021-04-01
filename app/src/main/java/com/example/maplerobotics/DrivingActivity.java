2e92521d8bd932c7552ca5ffcb363457342752f4
Johnny <johnnytsai97@gmail.com>
2021-03-31 17:04:06 -0700

    Added Temp log and TouchListeners on DrivingActivity

--- app/src/main/java/com/example/maplerobotics/DrivingActivity.java
+++ app/src/main/java/com/example/maplerobotics/DrivingActivity.java
@@ -2,7 +2,14 @@ package com.example.maplerobotics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class DrivingActivity extends AppCompatActivity {
 
@@ -10,5 +17,84 @@ public class DrivingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driving);
+
+        //TODO: Directional Buttons
+        Button forwardBtn = findViewById(R.id.forwardBtn);
+        Button leftBtn = findViewById(R.id.leftBtn);
+        Button rightBtn = findViewById(R.id.rightBtn);
+        Button reverseBtn = findViewById(R.id.reverseBtn);
+
+        forwardBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                TextView commandTextView = findViewById(R.id.commandTextView);
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // SEND COMMAND TO ARDUINO HERE
+                        commandTextView.setText("touching Forward");
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // SEND STOP COMMAND TO ARDUINO HERE
+                        commandTextView.setText("STOP");
+                        break;
+                }
+                return false;
+            }
+        });
+
+        leftBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                TextView commandTextView = findViewById(R.id.commandTextView);
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // SEND COMMAND TO ARDUINO HERE
+                        commandTextView.setText("touching Left");
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // SEND STOP COMMAND TO ARDUINO HERE
+                        commandTextView.setText("STOP");
+                        break;
+                }
+                return false;
+            }
+        });
+
+        rightBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                TextView commandTextView = findViewById(R.id.commandTextView);
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // SEND COMMAND TO ARDUINO HERE
+                        commandTextView.setText("touching Right");
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // SEND STOP COMMAND TO ARDUINO HERE
+                        commandTextView.setText("STOP");
+                        break;
+                }
+                return false;
+            }
+        });
+
+        reverseBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                TextView commandTextView = findViewById(R.id.commandTextView);
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // SEND COMMAND TO ARDUINO HERE
+                        commandTextView.setText("touching Back");
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // SEND STOP COMMAND TO ARDUINO HERE
+                        commandTextView.setText("STOP");
+                        break;
+                }
+                return false;
+            }
+        });
     }
+
 }
